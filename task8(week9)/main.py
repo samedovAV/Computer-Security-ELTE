f42fec61bfe8ba9a213441e200a5b04c001f98f2
samedovAV <alekcandr206@gmail.com>
2022-11-10 14:31:08 +0300

    week 9 start

---
+++ task8(week9)/main.py
@@ -0,0 +1,9 @@
+def sha256(message):
+    """
+    >>> sha256('I')
+    'a83dd0ccbffe39d071cc317ddf6e97f5c6b1c87af91919271f9fa140b0508c6c'
+    >>> sha256('love')
+    '686f746a95b6f836d7d70567c302c3f9ebb5ee0def3d1220ee9d4e9f34f5e131'
+    >>> sha256('crypto')
+    'da2f073e06f78938166f247273729dfe465bf7e46105c13ce7cc651047bf0ca4'
+    """
