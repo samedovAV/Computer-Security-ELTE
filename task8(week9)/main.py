a4f69cb99b1cd11ece9a4ba979982f4cc2825e09
samedovAV <alekcandr206@gmail.com>
2022-11-13 23:00:12 +0300

    week 9 start

--- task8(week9)/main.py
+++ task8(week9)/main.py
@@ -1,3 +1,7 @@
+import hashlib
+import itertools
+
+
 def sha256(message):
     """
     >>> sha256('I')
@@ -7,3 +11,49 @@ def sha256(message):
     >>> sha256('crypto')
     'da2f073e06f78938166f247273729dfe465bf7e46105c13ce7cc651047bf0ca4'
     """
+    return hashlib.sha256(message.encode('utf-8')).hexdigest()
+
+
+users = {
+    'admin': '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918',  # sha256('admin')
+    'user': '2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824',  # sha256('hello')
+}
+
+
+def authenticate(user, passwd):
+    """
+    >>> authenticate('admin','admin')
+    True
+    >>> authenticate('admin','admin2')
+    False
+    >>> authenticate('user','hello')
+    True
+    >>> authenticate('user','helo')
+    False
+    """
+    if user in users:
+        return users.get(user) == sha256(passwd)
+    else:
+        return False
+
+
+def hack_sha256_fixed_size(pwd_hash, pwd_len):
+    """
+    >>> hack_sha256_fixed_size('8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918',5)
+    'admin'
+    >>> hack_sha256_fixed_size('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824',5)
+    'hello'
+    >>> hack_sha256_fixed_size('a819d7cd38e9101be2e496298e8bf426ce9cdf78d2af35ddf44c6ad25d50158b',5)
+    'crypt'
+    >>> hack_sha256_fixed_size('688787d8ff144c502c7f5cffaafe2cc588d86079f9de88304c26b0cb99ce91c6',3)
+    'asd'
+    >>> hack_sha256_fixed_size('7ec658e98073955c48314d0146593497a163d79f4e1dfea4bab03b79af227214',4)
+    'elte'
+    """
+    complete_list = []
+    for current in range(10):
+        a = [i for i in pwd_hash]
+        for y in range(current):
+            a = [x + i for i in pwd_hash for x in a]
+        complete_list = complete_list + a
+    print(complete_list)
